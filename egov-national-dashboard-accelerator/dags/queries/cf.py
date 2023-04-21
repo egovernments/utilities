ce1f87317423d3f362ade0d85680bc14f10cdd61
Priyanka-eGov <priyanka@egovernments.org>
2023-04-21 17:25:01 +0530

    UM-6082 changes for Citizen Feedback DAG

---
+++ egov-national-dashboard-accelerator/dags/queries/cf.py
@@ -0,0 +1,218 @@
+
+import logging
+
+def extract_cf_total_no_of_citizen_responses_by_channel(metrics, region_bucket):
+    #TODO nested loop for aggregation to be implemented
+
+    # service_module_agg = region_bucket.get('serviceModule')
+    # service_module_buckets = service_module_agg.get('buckets')
+
+    # for service_module_bucket in service_module_buckets:
+    #   metrics['']=
+
+
+
+    # earlier channel based aggregation
+    # channel_agg = region_bucket.get('channel')
+    # channel_buckets = channel_agg.get('buckets')
+    # grouped_by = []
+
+    # for channel_bucket in channel_buckets:
+    #   grouped_by.append({'name': channel_bucket.get('key'),'value': 
+    #   channel_bucket.get('todaysNoOfCitizenResponses').get('value') if 
+    #   channel_bucket.get('todaysNoOfCitizenResponses') else 0})
+
+    # metrics['todaysNoOfCitizenResponses'] = [{'groupBy': 'channel', 'buckets': grouped_by}]
+    # return metrics
+
+
+cf_total_no_of_citizen_responses_by_channel = {'path': 'citizen-feedback/_search',
+                          'name': 'cf_total_no_of_citizen_responses_by_channel',
+                          'lambda': extract_cf_total_no_of_citizen_responses_by_channel,
+                          'query': """
+{{
+  "size": 0,
+  "query": {{
+    "bool": {{
+      "must": [
+        {{
+          "range": {{
+            "Data.@timestamp": {{
+              "gte": {0},
+              "lte": {1}},
+              "format": "epoch_millis"
+            }}
+          }}
+        }}
+      ],
+      "must_not": [
+        {{
+          "term": {{
+            "Data.tenantId.keyword": "pb.testing"
+          }}
+        }}
+      ]
+    }}
+  }},
+  "aggs": {{
+    "ward": {{
+      "terms": {{
+        "field": "Data.ward.name.keyword",
+        "size": 10000
+      }},
+      "aggs": {{
+        "ulb": {{
+          "terms": {{
+            "field": "Data.tenantId.keyword",
+            "size": 10000
+          }},
+          "aggs": {{
+            "region": {{
+              "terms": {{
+                "field": "Data.tenantData.city.districtName.keyword",
+                "size": 10000
+              }},
+              "aggs": {{
+                "channel": {{
+                  "terms": {{
+                    "field": "Data.channel.keyword",
+                    "size": 10000
+                  }},
+                  "aggs": {{
+                    "todaysNoOfCitizenResponsesForChannel": {{
+                      "value_count": {{
+                        "field": "Data.id.keyword"
+                      }}
+                    }}
+                  }}
+                }}
+              }}
+            }}
+          }}
+        }}
+      }}
+    }}
+  }}
+}}
+
+"""
+                          }
+
+def extract_average_citizen_rating(metrics, region_bucket):
+    metrics['todaysAverageCitizenRating'] = region_bucket.get('todaysAverageCitizenRating').get(
+        'value') if region_bucket.get('todaysAverageCitizenRating') else 0
+    return metrics
+
+
+average_citizen_rating = {'path': 'citizen-feedback/_search',
+                             'name': 'average_citizen_rating',
+                             'lambda': extract_average_citizen_rating,
+                             'query':
+                                 """
+{{
+  "size": 0,
+  "query": {{
+    "bool": {{
+      "must": [
+        {{
+          "range": {{
+            "Data.@timestamp": {{
+              "gte": {0},
+              "lte": {1},
+              "format": "epoch_millis"
+            }}
+          }}
+        }}
+      ],
+      "must_not": [
+        {{
+          "term": {{
+            "Data.tenantId.keyword": "pb.testing"
+          }}
+        }}
+      ]
+    }}
+  }},
+  "aggs": {{
+    "ward": {{
+      "terms": {{
+        "field": "Data.ward.name.keyword",
+        "size": 10000
+      }},
+      "aggs": {{
+        "ulb": {{
+          "terms": {{
+            "field": "Data.tenantId.keyword",
+            "size": 10000
+          }},
+          "aggs": {{
+            "region": {{
+              "terms": {{
+                "field": "Data.tenantData.city.districtName.keyword",
+                "size": 10000
+              }},
+              "aggs": {{
+                "NoOfCitizenResponses": {{
+                  "value_count": {{
+                    "field": "Data.id.keyword"
+                  }}
+                }},
+                "totalFeedbackScore": {{
+                  "sum": {{
+                    "field": "Data.rating"
+                  }}
+                }},
+                "todaysAverageCitizenRating": {{
+                  "bucket_script": {{
+                    "buckets_path": {{
+                      "closed": "NoOfCitizenResponses",
+                      "total": "totalFeedbackScore"
+                    }},
+                    "script": "params.total / params.closed"
+                  }}
+                }}
+              }}
+            }}
+          }}
+        }}
+      }}
+    }}
+  }}
+}}
+"""
+                             }
+
+cf_queries = [cf_total_no_of_citizen_responses_by_channel,
+              average_citizen_rating,
+               serviceModule, serviceType
+              ]
+
+#the default payload for CF
+def empty_cf_payload(region, ulb, ward, date ):
+    return {
+        "date": date,
+        "module": "CF",
+        "ward": ward,
+        "ulb": ulb,
+        "region": region,
+        "state": "Punjab",
+        "metrics": {
+            "serviceModule": "",
+            "serviceType": "",
+            "todaysNoOfCitizenResponses": [
+              "groupBy": "channel",
+               "buckets": [
+                        {
+                            "name": "",
+                            "value": 0
+                        },
+                        {
+                            "name": "2019-20",
+                            "value": 0
+                        }
+                        ]
+            ],
+            "todaysAverageCitizenRating": 0
+        }
+    }
+
